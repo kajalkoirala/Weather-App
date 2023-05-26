6f8d15f5a62cb05fa9adc1dcf3c404f301b7cceb
Kajal Koirala <99066338+kajalkoirala@users.noreply.github.com>
2023-05-26 22:22:02 +0545

    second commit

--- Weather App/home.js
+++ Weather App/home.js
@@ -1,5 +1,5 @@
 async function fetchWeather() {
-  const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+   const url = ('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city , options)
   const options = {
     method: 'GET',
     headers: {
@@ -9,25 +9,57 @@ async function fetchWeather() {
   };
   
   
+  const getWeather = async (city) => {
+    
+    cityName.innerHTML = city;
+  
     try {
-      const response = await fetch(url, options); 
+      const response = await fetch(url, options);
       const result = await response.json();
       console.log(result);
+  
+    
       cloud_pct.innerHTML = result.cloud_pct;
+  
+      
       temp.innerHTML = result.temp;
+  
+     
       feels_like.innerHTML = result.feels_like;
+  
+      
       humidity.innerHTML = result.humidity;
+  
       min_temp.innerHTML = result.min_temp;
+  
+      
       max_temp.innerHTML = result.max_temp;
+  
+ 
       wind_speed.innerHTML = result.wind_speed;
-      wind_degrees.innerHTML= result.wind_degrees;
+  
+      wind_degrees.innerHTML = result.wind_degrees;
+  
+    
       sunrise.innerHTML = result.sunrise;
+  
+    
       sunset.innerHTML = result.sunset;
-
-    } catch (error) {
+    } 
+    catch (error) {
       console.error(error);
     }
+  };
+
+  
+  
+    GO.addEventListener('click', (e) => {
+      e.preventDefault();
+      getWeather(city.value);
+    });
+  
+    getWeather('kathmandu');
   }
+
   
   fetchWeather();
-  
